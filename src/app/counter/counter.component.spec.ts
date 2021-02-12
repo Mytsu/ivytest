ae0d25575272f839008c45f38f95079da1642fa7
Mytsu <tlc.jooker@gmail.com>
2021-02-12 11:29:06 -0300

    removing some tests

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,17 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { CounterComponent } from './counter.component';
 
-describe('AppComponent', () => {
+describe('CounterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        CounterComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(CounterComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
